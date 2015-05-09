bb5ec56d6cfa5691115fd7d4a07ad1df4065e325
Josh Glendenning <joshglendenning@gmail.com>
2015-05-08 22:40:57 -0500

    Update 2015-05-09T03:40:56.019Z

--- services/api.js
+++ services/api.js
@@ -1,8 +1,8 @@
 import {HTTP} from 'http';
 import {vm} from 'main';
 
-const base = 'http://localhost:3000';
-//const base = 'https://branchr.herokuapp.com';
+const base = 'https://branchr.herokuapp.com';
+//const base = 'http://localhost:3000';
 
 var httpNoAuth = new HTTP();
 
